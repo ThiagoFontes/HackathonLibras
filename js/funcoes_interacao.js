331604fbe0178054d2d0f07cd1bc4d642839443f
Ikaro Feitosa <ikarosfeitosa@gmail.com>
2018-09-15 09:47:57 -0300

    Atualizando o front-end

---
+++ js/funcoes_interacao.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	$(".link_pesquisa").click(function(e) {
+		/* Act on the event */
+		e.preventDefault();
+		var link = $(this).attr('href');
+		
+		location.href=link;
+	});
+});
